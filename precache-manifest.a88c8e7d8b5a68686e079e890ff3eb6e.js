cdbb98a372e6cbda36087fcc8fd8427be71263d4
Nithishkumar1806199 <nithishkuma8123@gmail.com>
2020-09-03 17:42:33 +0530

    Updates

---
+++ precache-manifest.a88c8e7d8b5a68686e079e890ff3eb6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97ec8460504bc32c11aa350bb93b1bb",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "701c0c024be3b3433e9b",
+    "url": "/amazon-clone/static/css/main.d5f89e96.chunk.css"
+  },
+  {
+    "revision": "e866e5d51b67b60c27a7",
+    "url": "/amazon-clone/static/js/2.72195b59.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.72195b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701c0c024be3b3433e9b",
+    "url": "/amazon-clone/static/js/main.3133a1bf.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  },
+  {
+    "revision": "5f1429a184db5e19c9e64c1189a3c5b3",
+    "url": "/amazon-clone/static/media/logo.5f1429a1.png"
+  },
+  {
+    "revision": "2edaa6056fcffcfec7918109cafc5541",
+    "url": "/amazon-clone/static/media/logo2.2edaa605.png"
+  }
+]);
